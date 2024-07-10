2dde639fd4626e64f0d4fa851bd6abc817365b1d
AozoraDev <aozoradeveloper@gmail.com>
2024-07-11 02:28:46 +0800

    Revisi

---
+++ src/_data/faq.cjs
@@ -0,0 +1,29 @@
+module.exports = [{
+    q: "Berapa lama waktu yang dibutuhkan tim untuk menerjemahkan satu visual novel?",
+    a: "Tergantung niat dan waktu luang tim kami. Bisa saja berbulan-bulan atau bertahun-tahun."
+},
+{
+    q: "Bagaimana cara aku mengetahui sampai dimana progress [nama visual novel] sedang diterjemahkan?",
+    a: `Kadang (tapi tidak jarang) setiap bulan, kami memposting detail progress kerja tim kami di blog Project Honku.
+        <br>
+        Blog nya lagi dikerjakan sih, btw.`
+},
+{
+    q: `Maksud "Status" dan "Mesin" pada informasi proyek kalian itu apa?`,
+    a: `"Status" adalah status dari visual novel yang sedang diterjemahkan tim kami. Berikut penjelasannya:
+        <ul>
+            <li><span class="badge text-bg-dark">Planned</span> yang berarti visual novel tersebut direncanakan akan diterjemahkan.</li>
+            <li><span class="badge text-bg-info">In Progress</span> yang berarti visual novel tersebut sedang dalam proses penerjemahan.</li>
+            <li><span class="badge text-bg-success">Released</span> yang berarti terjemahan kami telah dirilis untuk publik dan dapat digunakan oleh mereka yang memiliki game originalnya.</li>
+            <li><span class="badge text-bg-danger">Dropped</span> yang berarti terjemahan kami telah dihapus. Biasanya karena masalah DMCA atau permintaan dari developer visual novel tersebut.</li>
+        </ul>
+        Sedangkan "Mesin" adalah jenis dari mesin permainan (game engine) yang digunakan oleh visual novel tersebut.
+        <br>
+        Contohnya seperti Ren'Py yang biasanya digunakan oleh visual novel barat dan Kirikiri yang biasanya digunakan oleh visual novel Jepang.`
+},
+{
+    q: "Aku ingin [nama visual novel] diterjemahkan. Boleh request gak?",
+    a: `Boleh banget!
+        <br>
+        Kami sedang mengerjakan formulir request buat kalian yang pengen visual novel kesukaannya diterjemahkan oleh kami.`
+}]
