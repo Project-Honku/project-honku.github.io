d498664f936313f81e707e574438a225dbe7f609
AozoraDev <aozoradeveloper@gmail.com>
2024-07-13 14:16:09 +0800

    Update faq

--- src/_data/faq.cjs
+++ src/_data/faq.cjs
@@ -14,6 +14,7 @@ module.exports = [{
         <ul>
             <li><span class="badge text-bg-dark">Planned</span> yang berarti visual novel tersebut direncanakan akan diterjemahkan.</li>
             <li><span class="badge text-bg-info">In Progress</span> yang berarti visual novel tersebut sedang dalam proses penerjemahan.</li>
+            <li><span class="badge text-bg-warning">Released (Partial)</span> yang berarti terjemahan kami telah dirilis untuk publik tetapi sebagian dialog masih belum diterjemahkan.</li>
             <li><span class="badge text-bg-success">Released</span> yang berarti terjemahan kami telah dirilis untuk publik dan dapat digunakan oleh mereka yang memiliki game originalnya.</li>
             <li><span class="badge text-bg-danger">Dropped</span> yang berarti terjemahan kami telah dihapus. Biasanya karena masalah DMCA atau permintaan dari developer visual novel tersebut.</li>
         </ul>
