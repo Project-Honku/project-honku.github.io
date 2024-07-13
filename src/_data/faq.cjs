309ca29d6f60005422dcb19f28d32e56342f5f07
AozoraDev <aozoradeveloper@gmail.com>
2024-07-13 16:03:18 +0800

    Fix mis

--- src/_data/faq.cjs
+++ src/_data/faq.cjs
@@ -16,7 +16,7 @@ module.exports = [{
             <li><span class="badge text-bg-info">In Progress</span> yang berarti visual novel tersebut sedang dalam proses penerjemahan.</li>
             <li><span class="badge text-bg-warning">Released (Partial)</span> yang berarti terjemahan kami telah dirilis untuk publik tetapi sebagian dialog masih belum diterjemahkan.</li>
             <li><span class="badge text-bg-success">Released</span> yang berarti terjemahan kami telah dirilis untuk publik dan dapat digunakan oleh mereka yang memiliki game originalnya.</li>
-            <li><span class="badge text-bg-danger">Dropped</span> yang berarti terjemahan kami telah dihapus. Biasanya karena masalah DMCA atau permintaan dari developer visual novel tersebut.</li>
+            <li><span class="badge text-bg-danger">Dropped</span> yang berarti terjemahan kami telah dihapus. Biasanya karena masalah DMCA atau permintaan dari pihak terkait.</li>
         </ul>
         Sedangkan "Mesin" adalah jenis dari mesin permainan (game engine) yang digunakan oleh visual novel tersebut.
         <br>
