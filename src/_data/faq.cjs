12eccf6aaa1246f17a51768c31345626ae9a808a
AozoraDev <aozoradeveloper@gmail.com>
2024-07-29 01:05:23 +0800

    Uhm...

--- src/_data/faq.cjs
+++ src/_data/faq.cjs
@@ -1,16 +1,16 @@
 module.exports = [{
-    q: "Berapa lama waktu yang dibutuhkan tim untuk menerjemahkan satu visual novel?",
-    a: "Tergantung niat dan waktu luang tim kami. Bisa saja berbulan-bulan atau bertahun-tahun."
+    q: "Biasanya tim butuh waktu berapa lama untuk menerjemahkan satu visual novel?",
+    a: "Yah tergantung niat dan waktu luang tim kami sih. Kadang berbulan-bulan atau bertahun-tahun."
 },
 {
-    q: "Bagaimana cara aku mengetahui sampai dimana progress [nama visual novel] sedang diterjemahkan?",
+    q: "Cara agar aku bisa tahu progress [nama visual novel] sedang diterjemahkan gimana yah?",
     a: `Kadang (tapi tidak jarang) setiap bulan, kami memposting detail progress kerja tim kami di blog Project Honku.
         <br>
         Blog nya lagi dikerjakan sih, btw.`
 },
 {
-    q: `Maksud "Status" dan "Mesin" pada informasi proyek kalian itu apa?`,
-    a: `"Status" adalah status dari visual novel yang diterjemahkan tim kami. Berikut penjelasannya:
+    q: `Maksud "Status" dan "Mesin" di informasi proyek itu apa yah?`,
+    a: `"Status" itu adalah status dari visual novel yang diterjemahkan tim kami. Berikut penjelasannya:
         <ul>
             <li><span class="badge text-bg-dark">Planned</span> yang berarti visual novel tersebut direncanakan akan diterjemahkan.</li>
             <li><span class="badge text-bg-info">In Progress</span> yang berarti visual novel tersebut sedang dalam proses penerjemahan.</li>
@@ -18,12 +18,12 @@ module.exports = [{
             <li><span class="badge text-bg-success">Released</span> yang berarti terjemahan kami telah dirilis untuk publik dan dapat digunakan oleh mereka yang memiliki game originalnya.</li>
             <li><span class="badge text-bg-danger">Dropped</span> yang berarti terjemahan kami telah dihapus. Biasanya karena masalah DMCA atau permintaan dari pihak terkait.</li>
         </ul>
-        Sedangkan "Mesin" adalah jenis dari mesin permainan (game engine) yang digunakan oleh visual novel tersebut.
+        Sedangkan "Mesin" adalah jenis dari mesin permainan (game engine) yang digunakan oleh visual novel terkait.
         <br>
         Contohnya seperti Ren'Py yang biasanya digunakan oleh visual novel barat dan Kirikiri yang biasanya digunakan oleh visual novel Jepang.`
 },
 {
-    q: "Aku ingin [nama visual novel] diterjemahkan. Boleh request gak?",
+    q: "Aku pengen [nama visual novel] diterjemahkan. Boleh request gak?",
     a: `Boleh banget!
         <br>
         Kami sedang mengerjakan formulir request buat kalian yang pengen visual novel kesukaannya diterjemahkan oleh kami.`
