9f08fbf357e623d4454681d001b85981820a64dd
AozoraDev <aozoradeveloper@gmail.com>
2024-10-24 01:55:56 +0800

    Simplify rating

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -13,7 +13,7 @@ module.exports = (config) => {
     config.addWatchTarget("src/blog/**/*");
     config.addPassthroughCopy("src/CNAME");
 
-    // Global Data
+    config.addShortcode("addRating", addRating);
     config.addShortcode("gotoCDN", (path) => "https://pixboost.com/api/2/img/https://cdn.honku.my.id" + path + "/optimise?auth=NDUyNzY3NzM3")
     config.addShortcode("gotoPro", (product) => "/products/" + product);
 
@@ -36,4 +36,39 @@ module.exports = (config) => {
         markdownTemplateEngine: "njk",
         htmlTemplateEngine: "njk"
     }
+}
+
+/**
+ * Add rating HTML
+ * 
+ * @param {number | string} rating - The rating of product
+ * @param {boolean?} censored - Is product censored?
+ * @param {boolean?} completed - Use long version of rating name
+ */
+function addRating(rating, censored = false, completed = false) {
+    let colorClass = "success";
+
+    switch (true) {
+        case rating >= 18:
+            colorClass = "danger";
+        break;
+        case rating >= 14:
+            colorClass = "warning";
+        break;
+        case rating >= 10:
+            colorClass = "info";
+        break;
+        default:
+            colorClass = "success";
+            rating = (completed) ? "Semua Umur" : "SU";
+        break;
+    }
+
+    let html = `<span class="badge text-bg-${colorClass}">`;
+    html += rating;
+    if (typeof rating == "number") html += "+";
+    if (censored) html += "&nbsp;(Versi Sensor)";
+    html += `</span>`;
+
+    return html;
 }
